9ec2cc7916c045b88eea475c392a42520e07b61c
kyum22n <aphrod207@naver.com>
2026-02-06 06:57:48 +0900

    feat: 파이썬 모듈&패키지

---
+++ module_test.py
@@ -0,0 +1,20 @@
+# 모듈 사용 실습
+
+import sys
+
+print(sys.path) #기본적인 파이썬 엔진이 출력할 경로 확인&등록
+
+print(type(sys.path))
+
+# 모듈 경로 삽입
+# sys.path.append('C:/Users/Metanet/Documents/kyumin/study/python/math')
+
+# print(sys.path)
+
+# import test_module
+
+# 모듈 사용
+# print(test_module.power(10, 3))
+
+import chapter06_02
+
